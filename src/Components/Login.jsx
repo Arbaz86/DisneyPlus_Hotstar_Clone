fa9167da640b716512726de9f852d7a967f824b8
Arbaz Hussain <arbazh777@gmail.com>
2023-01-24 13:06:13 +0530

    Added required IMAGES and VIDEOS

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,24 +1,22 @@
-import { Box, Stack, Flex } from "@chakra-ui/react";
+import { Box, VStack, Flex } from "@chakra-ui/react";
 import React from "react";
 
 export const Login = () => {
   return (
-    <Stack
-      overflow="hidden"
-      flexDirection="column"
-      textAlign="center"
-      height="100vh"
-    >
+    <VStack overflow="hidden" textAlign="center" height="100vh">
       <Flex
+        direction="column"
         w="100%"
         position="relative"
         minH="100vh"
         justifyContent="center"
         alignItems="center"
         mb="10vw"
+        p="80px 40px"
+        h="100%"
       >
         Content
       </Flex>
-    </Stack>
+    </VStack>
   );
 };
